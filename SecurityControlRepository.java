3405648182f44187b7c625cda8b8271b8a80824d
AdamFitzgibbon3108 <148885141+AdamFitzgibbon3108@users.noreply.github.com>
2025-02-13 12:58:53 +0000

    Update SecurityControlRepository.java

--- SecurityControlRepository.java
+++ SecurityControlRepository.java
@@ -8,3 +8,4 @@ import org.springframework.stereotype.Repository;
 public interface SecurityControlRepository extends JpaRepository<SecurityControl, Long> {
     SecurityControl findByName(String name);
 }
+
